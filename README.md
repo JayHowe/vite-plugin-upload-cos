39b0340db022269564be0060f6c8c438a7ed0921
jayhowe <jayhowe2014@163.com>
2021-11-09 17:27:20 +0800

    feat: init vite plugin

---
+++ README.md
@@ -0,0 +1,4 @@
+# vite-plugin-upload-cos
+vite-plugin-upload-cos,  vite插件，兼容rollup插件，cos插件， 打包构建完成之后将资源上传到cos， 可以在vite或者rollup中使用
+
+
---
+++ index.js
@@ -0,0 +1,73 @@
+const COS = require('cos-nodejs-sdk-v5');
+/**
+ * vite-plugin-upload-cos 静态资源上传cos rollup插件
+ * @SecretId String 腾讯云cos SecretId
+ * @SecretKey String 腾讯云cos SecretKey
+ * @bucket String 腾讯云cos bucket
+ * @region String 腾讯云cos region
+ * @cosBaseDir String 腾讯云cos存放的资源的地址
+ * @uploadDir String 选填，腾讯云cos存放的资源的地址
+ * @excludes []String 选填，不需要上传的文件数组
+ * 
+ * @returns 
+ */
+ export default function upload2Cos({
+  SecretId,
+  SecretKey,
+  bucket,
+  region,
+  cosBaseDir,
+  uploadDir = 'dist',
+  excludes = []
+}) {
+  const cosObject = new COS({
+    SecretId,
+    SecretKey,
+  });
+  return {
+    name: 'vite-plugin-upload-cos',
+    async writeBundle(_option, bundles) {
+      const uploadfiles = Object.keys(bundles).filter((item) => !excludes.includes(item));
+      console.log('!!bundles', uploadfiles);
+
+      function upload2CosPromise({ url }) {
+        return new Promise((resolve) => {
+          cosObject.putObject(
+            {
+              Bucket: bucket,
+              Region: region,
+              Key: `${cosBaseDir}/${url}`,
+              StorageClass: 'STANDARD',
+              Body: fs.createReadStream(`./${uploadDir}/${url}`),
+              onProgress: function() {
+                  console.log(`\x1B[36m[vite-plugin-upload-cos]Uploading ${url}\x1B[0m`)
+              }
+            },
+            function (err) {
+              if (err) {
+                resolve(`Upload ${url} error: ${JSON.stringify(err)}`);
+              } else {
+                resolve(`${url} Uploaded!`);
+              }
+            },
+          );
+        });
+      }
+
+      const files = [];
+      uploadfiles.forEach((item) => {
+        const promiseFile = upload2CosPromise({
+          url: item,
+        });
+        files.push(promiseFile);
+      });
+      Promise.all(files)
+        .then((res) => {
+          console.log('[vite-plugin-upload-cos]Upload to COS finish: ', res);
+        })
+        .catch((err) => {
+          console.log('[vite-plugin-upload-cos]Upload to COS fails: ', err);
+        });
+    },
+  };
+}
